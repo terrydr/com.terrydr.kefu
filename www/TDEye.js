abd693243dfe5272279f40acef7e857876cb18ff
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-06-30 10:10:18 +0800

    fix js

--- www/TDEye.js
+++ www/TDEye.js
@@ -1,42 +1,16 @@
-var tdEye= {
-tdTakePhotos: function(takeType, successCallback, errorCallback) {
-    console.log("tdTakePhotos");
+var tdKefu= {
+tdConnectKefu: function(successCallback, errorCallback) {
+    console.log("tdConnectKefu");
     cordova.exec(
                  successCallback,
                  errorCallback,
-                 "TDEye",
-                 "tdEyeTakePhotos",
-                 [takeType]
-                 );
-    
-}
-    ,
- 
-tdSelectPhotos: function(successCallback, errorCallback) {
-    console.log("tdSelectPhotos");
-    cordova.exec(
-                 successCallback,
-                 errorCallback,
-                 "TDEye",
-                 "tdEyeSelectPhotos",
+                 "TDKefu",
+                 "tdConnectKefu",
                  []
-                 );
-    
-}
-   ,
-               
-tdScanPhotos: function(paramDic,successCallback, errorCallback) {
-    console.log("tdScanPhotos");
-    cordova.exec(
-                 successCallback,
-                 errorCallback,
-                 "TDEye",
-                 "tdEyeScanPhotos",
-                 [paramDic]
                  );
     
 }
     
 }
 
-module.exports = tdEye;
+module.exports = tdKefu;
