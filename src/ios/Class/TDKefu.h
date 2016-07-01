fc17f465d2234b2bec1b88d89fc457fbbec7b290
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-07-01 11:17:47 +0800

    fix bug

--- src/ios/Class/TDKefu.h
+++ src/ios/Class/TDKefu.h
@@ -10,6 +10,7 @@
 
 @interface TDKefu : CDVPlugin
 
+- (void)tdRegisterKefu:(CDVInvokedUrlCommand*)command;
 - (void)tdConnectKefu:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/Class/TDKefu.m
+++ src/ios/Class/TDKefu.m
@@ -9,13 +9,28 @@
 #import "TDKefu.h"
 #import "QYSDK.h"
 
+//七鱼客服
+#define QYSERVICE_KEY         @"c1babe7f57e28c009565369c1da798bf"
+#define RGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
+
 @implementation TDKefu
+
+- (void)tdRegisterKefu:(CDVInvokedUrlCommand*)command{
+    [[QYSDK sharedSDK] registerAppId:QYSERVICE_KEY appName:@"泰立瑞"];
+}
 
 - (void)tdConnectKefu:(CDVInvokedUrlCommand*)command{
     QYSessionViewController *sessionViewController = [[QYSDK sharedSDK] sessionViewController];
     sessionViewController.sessionTitle = @"泰立瑞";
     sessionViewController.hidesBottomBarWhenPushed = YES;
-    [self.viewController.navigationController pushViewController:sessionViewController animated:YES];
+    
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:sessionViewController];
+    nav.navigationBar.translucent = NO;
+    nav.navigationBar.barTintColor = RGB(0x3691e6);
+    nav.navigationBar.tintColor = [UIColor whiteColor];
+    [nav.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, [UIFont boldSystemFontOfSize:18.f], NSFontAttributeName, nil]];
+    [self.viewController presentViewController:nav animated:YES completion:^{
+    }];
     
     QYUserInfo *userInfo = [[QYUserInfo alloc] init];
     userInfo.userId = [NSString stringWithFormat:@"0_%@",@"18639027026"];
