1a7de43fce1da8b3f2640835aebe6a43432b04c1
路亮亮 <luliangliang@luliangliangdeMac-mini.local>
2016-07-01 17:29:21 +0800

    fix js

--- www/TDKefu.js
+++ www/TDKefu.js
@@ -1,25 +1,25 @@
 var tdKefu= {
-tdConnectKefu: function(paramDic,successCallback, errorCallback) {
+tdConnectKefu: function(sessionTitle,userId,userData,successCallback, errorCallback) {
     console.log("tdConnectKefu");
     cordova.exec(
                  successCallback,
                  errorCallback,
                  "TDKefu",
                  "tdConnectKefu",
-                 [paramDic]
+                 [sessionTitle,userId,userData]
                  );
     
 }
                ,
                
-tdRegisterKefu: function(serviceKey,successCallback, errorCallback) {
+tdRegisterKefu: function(serviceKey,appName,successCallback, errorCallback) {
     console.log("tdRegisterKefu");
     cordova.exec(
                  successCallback,
                  errorCallback,
                  "TDKefu",
                  "tdRegisterKefu",
-                 [serviceKey]
+                 [serviceKey,appName]
                  );
                
                }
